667eabbbb7df44f1bcc14890a370eda1e88a54a3
Krody Robert <krody.robi@gmail.com>
2014-12-30 11:08:42 +0200

    Added final version of LED

--- pi-sense/LED.py
+++ pi-sense/LED.py
@@ -1,10 +1,16 @@
 import time
 import RPi.GPIO as gpio
 
-class LED:
-    'Rasbery led class'
 
+class LED(object):
     def __init__(self, pin, mode=gpio.BCM):
+        """
+            Creates a LED instance for use on a RasberyPI B+
+
+            @param pin: Pin identification code for the selected mode
+            @type pin: int
+            @param mode: Can be gpio.BCM or gpio.BOARD
+        """
         self.pin = pin
         
         if mode == gpio.BCM or mode == gpio.BOARD:
@@ -12,16 +18,35 @@ class LED:
         
         gpio.setup(pin, gpio.OUT)
         gpio.setwarnings(False)
-        
-        
+
     def on(self):
+        """
+            Turns the led ON if it isn't already
+
+            @param self:
+            @return: None
+        """
         gpio.output(self.pin, True)
     
     def off(self):
+        """
+            Turns the led OFF if it isn't already
+
+            @param self:
+            @return : None
+        """
         gpio.output(self.pin, False)
-    
-    
-    def blink(self, time=1):
+
+    def blink(self, delay=1):
+        """
+            Turns the led ON and OFF with a specified delay ONCE
+
+            @param delay: Number of seconds between state transition
+            @type delay: float
+
+            @return: None
+        """
         gpio.output(self.pin, True)
-        time.sleep(time)
+        time.sleep(delay)
         gpio.output(self.pin, False)
+        time.sleep(delay)
